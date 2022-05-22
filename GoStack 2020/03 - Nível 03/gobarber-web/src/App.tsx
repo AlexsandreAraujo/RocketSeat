66f1b2d39a09a139e18ba48f215420a53b89f5df
Alexsandre Araujo <alexsandre.lima.araujo@outlook.com>
2022-05-22 01:34:04 -0300

    Isolando componentes

--- GoStack 2020/03 - Nível 03/gobarber-web/src/App.tsx
+++ GoStack 2020/03 - Nível 03/gobarber-web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
+import SingIn from './pages/SignIn';
 
 const App: React.FC = () => (
     <>
-        <h1>Hello world</h1>
+        <SingIn />
         <GlobalStyle />
     </>
 );
