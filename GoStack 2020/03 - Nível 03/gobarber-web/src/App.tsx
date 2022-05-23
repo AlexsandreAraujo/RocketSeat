da856598566408fd8058a153360b01cedfba5d2d
Alexsandre Araujo <alexsandre.lima.araujo@outlook.com>
2022-05-22 22:34:01 -0300

    Página de cadastro

--- GoStack 2020/03 - Nível 03/gobarber-web/src/App.tsx
+++ GoStack 2020/03 - Nível 03/gobarber-web/src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 import SingIn from './pages/SignIn';
+import SingUp from './pages/Signup';
 
 const App: React.FC = () => (
     <>
-        <SingIn />
+        <SingUp />
         <GlobalStyle />
     </>
 );
