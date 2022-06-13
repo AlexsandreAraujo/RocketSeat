9b2b857595410486da34767f0718dd3604b9d906
Alexsandre Araujo <alexsandre.lima.araujo@outlook.com>
2022-06-13 00:42:40 -0300

    Animando Toasts

--- GoStack 2020/03 - Nível 03/gobarber-web/src/components/ToastContainer/index.tsx
+++ GoStack 2020/03 - Nível 03/gobarber-web/src/components/ToastContainer/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTransition } from 'react-spring';
 import Toast from './Toast';
 import Container from './styles';
 import { ToastMessage } from '../../hooks/toast';
@@ -8,11 +9,16 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+    const messagesWithTransitions = useTransition(messages, {
+        from: { right: '-120%' },
+        enter: { right: '0%' },
+        leave: { right: '-120%' },
+    });
     return (
         <Container>
-            {messages.map(message => (
-                <Toast key={message.id} message={message} />
-            ))}
+            {messagesWithTransitions((props, item) => {
+                return <Toast style={props} message={item} />;
+            })}
         </Container>
     );
 };
