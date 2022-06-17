d6ee32cdd1caf11b130721766a6f06d8ea26496b
Alexsandre Araujo <alexsandre.lima.araujo@outlook.com>
2022-06-17 13:46:48 -0300

    Configurando navegação

--- GoStack 2020/03 - Nivel 03/appgobarber/src/App.tsx
+++ GoStack 2020/03 - Nivel 03/appgobarber/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-
 import { StatusBar, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-const app: React.FC = () => (
-  <>
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
-  </>
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
 );
 
-export default app;
+export default App;
