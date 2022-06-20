9e2b5e154d62f1ff05c037f0bd624f59f3edd1b9
Alexsandre Araujo <alexsandre.lima.araujo@outlook.com>
2022-06-20 11:47:49 -0300

    Input & Button

--- GoStack 2020/03 - Nivel 03/appgobarber/src/pages/SignIn/Index.tsx
+++ GoStack 2020/03 - Nivel 03/appgobarber/src/pages/SignIn/Index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
@@ -9,6 +11,15 @@ const SignIn: React.FC = () => {
       <Image source={logoImg} />
 
       <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button
+        onPress={() => {
+          console.log('Deu!');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
