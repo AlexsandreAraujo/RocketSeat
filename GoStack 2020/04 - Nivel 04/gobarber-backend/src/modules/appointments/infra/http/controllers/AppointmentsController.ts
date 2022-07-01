5b05ded2db405913ae3ea32793e6d7342c153d7a
Alexsandre Araujo <alexsandre.lima.araujo@outlook.com>
2022-07-01 12:10:34 -0300

    Usando controllers

---
+++ GoStack 2020/04 - Nivel 04/gobarber-backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { parseISO } from 'date-fns';
+import { container } from 'tsyringe';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+export default class AppointmentsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { providerId, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const CreateAppointment = container.resolve(CreateAppointmentService);
+
+    const appointment = await CreateAppointment.execute({
+      date: parsedDate,
+      providerId,
+    });
+
+    return response.json(appointment);
+  }
+
+  public async list(request: Request, response: Response): Promise<Response> {
+    const appointments = await Appointment.find();
+
+    return response.json(appointments);
+  }
+}
--- GoStack 2020/04 - Nivel 04/gobarber-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ GoStack 2020/04 - Nivel 04/gobarber-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,34 +1,15 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
-import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import AppointmentsController from '../controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 // Rotas devem Receber a requisição, chamar outro arquivo, devolver uma resposta
 
-appointmentsRouter.get('/', async (request, response) => {
-  const appointments = await Appointment.find();
+appointmentsRouter.get('/', appointmentsController.list);
 
-  return response.json(appointments);
-});
-
-appointmentsRouter.post('/', async (request, response) => {
-  const { providerId, date } = request.body;
-
-  const parsedDate = parseISO(date);
-
-  const CreateAppointment = container.resolve(CreateAppointmentService);
-
-  const appointment = await CreateAppointment.execute({
-    date: parsedDate,
-    providerId,
-  });
-
-  return response.json(appointment);
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
