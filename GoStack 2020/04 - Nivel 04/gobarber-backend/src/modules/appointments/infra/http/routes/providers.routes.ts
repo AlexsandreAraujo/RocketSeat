e0f467a2be5136a79c10df776b795e7713ddddf7
Alexsandre Araujo <alexsandre.lima.araujo@outlook.com>
2022-08-18 09:26:03 -0300

    Rotas e controllers

--- GoStack 2020/04 - Nivel 04/gobarber-backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ GoStack 2020/04 - Nivel 04/gobarber-backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,27 @@
 import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController =
+  new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController =
+  new ProviderDayAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 // Rotas devem Receber a requisição, chamar outro arquivo, devolver uma resposta
 
 providersRouter.get('/', providersController.index);
+providersRouter.get(
+  '/:id/month-availability',
+  providerMonthAvailabilityController.index,
+);
+providersRouter.get(
+  '/:id/day-availability',
+  providerDayAvailabilityController.index,
+);
 
 export default providersRouter;
