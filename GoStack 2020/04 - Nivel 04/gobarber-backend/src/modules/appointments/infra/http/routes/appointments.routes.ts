5870fa6ce4efe13f5b6f4f17dc81a1cedb1bf024
Alexsandre Araujo <alexsandre.lima.araujo@outlook.com>
2022-08-18 10:37:44 -0300

    Agenda do prestador

--- GoStack 2020/04 - Nivel 04/gobarber-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ GoStack 2020/04 - Nivel 04/gobarber-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 // Rotas devem Receber a requisição, chamar outro arquivo, devolver uma resposta
@@ -11,5 +13,7 @@ appointmentsRouter.use(ensureAuthenticated);
 appointmentsRouter.get('/', appointmentsController.list);
 
 appointmentsRouter.post('/', appointmentsController.create);
+
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
