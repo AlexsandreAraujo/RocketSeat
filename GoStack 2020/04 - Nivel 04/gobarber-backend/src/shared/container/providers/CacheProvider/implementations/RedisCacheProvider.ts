aedab18169c1dc96698650ae749a566ad57dfaee
Alexsandre Araujo <alexsandre.lima.araujo@outlook.com>
2022-08-20 16:04:19 -0300

    Cache lista de providers

---
+++ GoStack 2020/04 - Nivel 04/gobarber-backend/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -0,0 +1,29 @@
+import Redis, { Redis as RedisClient } from 'ioredis';
+import cache from '@config/cache';
+import ICacheProvider from '../models/ICacheProvider';
+
+export default class RedisCacheProvider implements ICacheProvider {
+  private client: RedisClient;
+
+  constructor() {
+    this.client = new Redis(cache.config.redis);
+  }
+
+  public async save(key: string, value: any): Promise<void> {
+    await this.client.set(key, JSON.stringify(value));
+  }
+
+  public async recovery<T>(key: string): Promise<T | null> {
+    const data = await this.client.get(key);
+
+    if (!data) {
+      return null;
+    }
+
+    const parsedData = JSON.parse(data) as T;
+
+    return parsedData;
+  }
+
+  public async invalidate(key: string): Promise<void> {}
+}
